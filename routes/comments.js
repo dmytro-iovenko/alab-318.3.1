aeb1f7903b39edf5c510f5bc2a6f016d36386844
Dmytro Iovenko <iovenko@gmail.com>
2024-09-04 22:09:29 -0400

    add [POST /comments]

--- routes/comments.js
+++ routes/comments.js
@@ -16,6 +16,20 @@ router
       },
     ];
     res.json({ comments, links });
+  })
+  .post((req, res, next) => {
+    if (req.body.userId && req.body.postId && req.body.body) {
+      const comment = {
+        id: comments[comments.length - 1]
+          ? comments[comments.length - 1].id + 1
+          : 1,
+        userId: req.body.userId,
+        postId: req.body.postId,
+        body: req.body.body,
+      };
+      comments.push(comment);
+      res.json(comments);
+    } else next(error(400, "Insufficient Data"));
   });
 
 module.exports = router;
